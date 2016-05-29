f1fb4dc01dde83a77c46b556b211f8ec00d05042
Jian-Hua, He <game09116@gmail.com>
2016-05-29 12:13:34 +0800

    Remove SubjectInterface instead use Subject abstract class

---
+++ src/Observer/Subjects/Subject.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Src\Observer\Subjects;
+
+abstract class Subject
+{
+    protected $observers = [];
+
+    public abstract function registerObserver();
+    public abstract function removeObserver();
+    public abstract function notifyObserver();
+}
