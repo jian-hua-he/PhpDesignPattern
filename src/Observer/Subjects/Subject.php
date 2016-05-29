e9e3e0d25c14382a3c5bb43aa4602927929e3d4f
Jian-Hua, He <game09116@gmail.com>
2016-05-29 23:17:38 +0800

    Fix missing parameters in Subject class

--- src/Observer/Subjects/Subject.php
+++ src/Observer/Subjects/Subject.php
@@ -2,11 +2,13 @@
 
 namespace Src\Observer\Subjects;
 
+use Src\Observer\Observers\ObserverInterface;
+
 abstract class Subject
 {
     protected $observers = [];
 
-    public abstract function registerObserver();
-    public abstract function removeObserver();
+    public abstract function registerObserver(ObserverInterface $observer);
+    public abstract function removeObserver(ObserverInterface $observer);
     public abstract function notifyObservers();
 }
