87633f34c9519d02df7d1a38b6a4f3606451048e
Jian-Hua, He <game09116@gmail.com>
2016-05-29 23:48:14 +0800

    All together

--- src/Observer/main.php
+++ src/Observer/main.php
@@ -3,11 +3,20 @@
 require '../../vendor/autoload.php';
 
 use Src\Observer\Subjects\WeatherSubject;
+use Src\Observer\Subjects\NewsSubject;
 use Src\Observer\Observers\SmartPhone;
 
+$jianhuaSmartPhone = new SmartPhone('Jian-Hua, He');
+$yahueiSmartPhone = new SmartPhone('Ya-Huei, Wu');
+
 $weatherSubject = new WeatherSubject;
-
-$smartPhone = new SmartPhone('Jian-Hua, He');
-
-$weatherSubject->registerObserver($smartPhone);
+$weatherSubject->registerObserver($jianhuaSmartPhone);
 $weatherSubject->updateMeasurements(30, 40, 1012);
+
+$newsSubject = new NewsSubject;
+$newsSubject->registerObserver($jianhuaSmartPhone);
+$newsSubject->registerObserver($yahueiSmartPhone);
+$newsSubject->updateNews('Obama go to Hiroshima', 'Love and Peace');
+
+$newsSubject->removeObserver($jianhuaSmartPhone);
+$newsSubject->updateNews('ES6 is 99% support by chrome!', 'That is awesome!');
