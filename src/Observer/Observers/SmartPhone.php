8e3e7189f70535eee7215af0411263e8ca6a1433
Jian-Hua, He <game09116@gmail.com>
2016-05-29 23:40:58 +0800

    Add new display type

--- src/Observer/Observers/SmartPhone.php
+++ src/Observer/Observers/SmartPhone.php
@@ -5,6 +5,7 @@ namespace Src\Observer\Observers;
 use Src\Observer\SubjectDatas\SubjectDataInterface;
 use Src\Observer\Displays\DisplayInterface;
 use Src\Observer\SubjectDatas\WeatherData;
+use Src\Observer\SubjectDatas\NewsData;
 
 class SmartPhone implements ObserverInterface, DisplayInterface
 {
@@ -22,11 +23,16 @@ class SmartPhone implements ObserverInterface, DisplayInterface
 
     public function display(SubjectDataInterface $data)
     {
+        echo "Smart Phone owner: {$this->owner}\n";
+
         if ($data instanceof WeatherData) {
-            echo "Smart Phone owner: {$this->owner}\n";
             echo "Temperature is: {$data->temperature}\n";
             echo "Humidity is: {$data->humidity}\n";
             echo "Pressure is: {$data->pressure}\n";
+            echo "\n";
+        } else if ($data instanceof NewsData)
+            echo "News title: {$data->title}\n";
+            echo "{$data->content}\n";
             echo "\n";
         } else {
             echo "You put wrong data\n";
