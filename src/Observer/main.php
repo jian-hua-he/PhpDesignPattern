d40748ac53f14524868cabf2bdcd9fea2bb0f2b6
Jian-Hua, He <game09116@gmail.com>
2016-05-29 23:18:40 +0800

    Put all together

--- src/Observer/main.php
+++ src/Observer/main.php
@@ -2,4 +2,12 @@
 
 require '../../vendor/autoload.php';
 
-echo "You got it\n";
+use Src\Observer\Subjects\WeatherSubject;
+use Src\Observer\Observers\SmartPhone;
+
+$weatherSubject = new WeatherSubject;
+
+$smartPhone = new SmartPhone('Jian-Hua, He');
+
+$weatherSubject->registerObserver($smartPhone);
+$weatherSubject->updateMeasurements(30, 40, 1012);
