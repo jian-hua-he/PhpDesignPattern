05b1f2a5529809b2011320971806be80c552eb7b
Jian-Hua, He <game09116@gmail.com>
2016-06-12 19:11:35 +0800

    Use pizza factory to create pizza

--- src/Factory/main.php
+++ src/Factory/main.php
@@ -2,4 +2,19 @@
 
 require '../../vendor/autoload.php';
 
-echo "Hello Factory Pattern\n";
+use Src\Factory\Factories\StuffedPizzaFactory;
+use Src\Factory\Factories\ThinCrustPizzaFactory;
+
+$stuffedPizzaFactory = new StuffedPizzaFactory;
+
+$stuffedCheesePizza = $stuffedPizzaFactory->createPizza('cheese');
+$stuffedClamPizza = $stuffedPizzaFactory->createPizza('clam');
+$stuffedPepperoniPizza = $stuffedPizzaFactory->createPizza('pepperoni');
+$stuffedVeggiePizza = $stuffedPizzaFactory->createPizza('veggie');
+
+$thinCrustPizzaFactory = new ThinCrustPizzaFactory;
+
+$thinCrustCheesePizza = $thinCrustPizzaFactory->createPizza('cheese');
+$thinCrustClamPizza = $thinCrustPizzaFactory->createPizza('clam');
+$thinCrustPepperoniPizza = $thinCrustPizzaFactory->createPizza('pepperoni');
+$thinCrustVeggiePizza = $thinCrustPizzaFactory->createPizza('veggie');
