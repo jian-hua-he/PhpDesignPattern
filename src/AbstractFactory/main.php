9dc400b27b4e35e69cacb5c33eb5e99cada36ff6
Jian-Hua, He <game09116@gmail.com>
2016-06-26 14:31:45 +0800

    All together

--- src/AbstractFactory/main.php
+++ src/AbstractFactory/main.php
@@ -2,4 +2,22 @@
 
 require '../../vendor/autoload.php';
 
-echo "Abstract Factory\n";
+use Src\AbstractFactory\ComponentFactories\GemanyComponentFactory;
+use Src\AbstractFactory\ComponentFactories\JapanComponentFactory;
+use Src\AbstractFactory\Cars\Car;
+
+$japanComponentFactory = new JapanComponentFactory;
+$gemanyComponentFactory = new GemanyComponentFactory;
+
+$japanTire = $japanComponentFactory->createTire();
+$japanCarBody = $japanComponentFactory->createCarBody();
+$japanCar = new Car($japanTire, $japanCarBody);
+echo "{$japanCar->drive()}\n";
+
+$gemanyTire = $gemanyComponentFactory->createTire();
+$gemanyCarBody = $gemanyComponentFactory->createCarBody();
+$gemanyCar = new Car($gemanyTire, $gemanyCarBody);
+echo "{$gemanyCar->drive()}\n";
+
+$mixinCar = new Car($japanTire, $gemanyCarBody);
+echo "{$mixinCar->drive()}\n";
