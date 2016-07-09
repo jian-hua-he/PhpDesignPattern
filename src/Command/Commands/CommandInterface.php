7c50923f0a1e6f45fc918b796a1fac88664d23e7
Jian-Hua, He <game09116@gmail.com>
2016-07-09 23:22:54 +0800

    Add LightOffCommand and LightOnCommand

--- src/Command/Commands/CommandInterface.php
+++ src/Command/Commands/CommandInterface.php
@@ -5,5 +5,4 @@ namespace Src\Command\Commands;
 interface CommandInterface
 {
     public function execute();
-    public function undo();
 }
---
+++ src/Command/Commands/LightOnCommand.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Src\Command\Commands;
+
+use Src\Command\Receivers\ReceiverInterface;
+
+class LightOnCommand implements CommandInterface
+{
+    private $light;
+
+    public function __construct(ReceiverInterface $light)
+    {
+        $this->light = $light;
+    }
+
+    public function execute()
+    {
+        return $light->on();
+    }
+}
