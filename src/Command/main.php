b597e68c0c1fa542266fdc293e388584e8bd9834
Jian-Hua, He <game09116@gmail.com>
2016-07-09 23:44:46 +0800

    Put all together

--- src/Command/main.php
+++ src/Command/main.php
@@ -2,4 +2,17 @@
 
 require '../../vendor/autoload.php';
 
+use Src\Command\Receivers\Light;
+use Src\Command\Commands\LightOnCommand;
+use Src\Command\Commands\LightOffCommand;
+use Src\Command\Controls\LightControl;
+
+$light = new Light;
+$lightOnCommand = new LightOnCommand($light);
+$LightOffCommand = new LightOffCommand($light);
+$lightControl = new LightControl($lightOnCommand, $LightOffCommand);
+
+$lightControl->pressOnButton();
+$lightControl->pressOffButton();
+
 echo "Command Pattern\n";
