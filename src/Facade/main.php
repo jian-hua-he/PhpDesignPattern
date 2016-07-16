cf52ab9807f295f617b02a28adc5224e1e9d82a8
Jian-Hua, He <game09116@gmail.com>
2016-07-17 00:12:28 +0800

    Put all together

--- src/Facade/main.php
+++ src/Facade/main.php
@@ -2,4 +2,9 @@
 
 require '../../vendor/autoload.php';
 
-echo "Facade\n";
+use Src\Facade\Facades\HomeTheaterFacade;
+
+$theater = new HomeTheaterFacade;
+$dvd = 'Star War';
+$theater->watchDVD($dvd);
+$theater->endWatch();
