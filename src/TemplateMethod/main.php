4272441b9e8523be09033c2523a8796e8867f541
Jian-Hua, He <game09116@gmail.com>
2016-07-17 15:41:54 +0800

    Put all together

--- src/TemplateMethod/main.php
+++ src/TemplateMethod/main.php
@@ -2,4 +2,13 @@
 
 require "../../vendor/autoload.php";
 
-echo "Template Method\n";
+use Src\TemplateMethod\Beverages\Tea;
+use Src\TemplateMethod\Beverages\Coffee;
+
+echo "Start to make a tea...\n";
+$tea = new Tea;
+$tea->prepareRecipe();
+
+echo "Start to make a coffee...\n";
+$coffee = new Coffee;
+$coffee->prepareRecipe();
