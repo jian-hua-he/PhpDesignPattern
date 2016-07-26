afe2e6a1ceadb20aa2fd195df5e27a84141008d9
Jian-Hua, He <game09116@gmail.com>
2016-07-26 23:44:34 +0800

    Add MenuComponent class

---
+++ src/Component/Menus/MenuComponent.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Src\Component\Menus;
+
+use Exception;
+
+class MenuComponent
+{
+    public function add(MenuComponent $menuComponent)
+    {
+        throw new Exception('Not implemented');
+    }
+
+    public function remove(MenuComponent $menuComponent)
+    {
+        throw new Exception('Not implemented');
+    }
+
+    public function getChild($index)
+    {
+        throw new Exception('Not implemented');
+    }
+
+    public function getName()
+    {
+        throw new Exception('Not implemented');
+    }
+
+    public function getDescription()
+    {
+        throw new Exception('Not implemented');
+    }
+
+    public function getPrice()
+    {
+        throw new Exception('Not implemented');
+    }
+
+    public function isVegetarian()
+    {
+        throw new Exception('Not implemented');
+    }
+
+    public function print()
+    {
+        throw new Exception('Not implemented');
+    }
+}
