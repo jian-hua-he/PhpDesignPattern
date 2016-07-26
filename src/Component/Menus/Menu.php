50e6d2d1e6b23abdf25751571d9b233f5710ec30
Jian-Hua, He <game09116@gmail.com>
2016-07-27 00:12:04 +0800

    Add Menu class

---
+++ src/Component/Menus/Menu.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Src\Component\Menus;
+
+use Exception;
+
+class Menu extends MenuComponent
+{
+    private $menuComponents = [];
+    private $name;
+    private $description;
+
+    public function __construct($name, $description)
+    {
+        $this->name = $name;
+        $this->description = $description;
+    }
+
+    public function add(MenuComponent $menuComponent)
+    {
+        $this->menuComponents[] = $menuComponent;
+    }
+
+    public function remove(MenuComponent $menuComponent)
+    {
+        if ($key = array_search($menuComponent, $this->menuComponents)) {
+            unset($this->menuComponents[$key]);
+            return;
+        }
+
+        throw new Exception('Menu not found');
+    }
+
+    public function getChild($index)
+    {
+        if ($index >= count($this->menuComponents)) {
+            throw new Exception('Invalid key in array');
+        }
+
+        return $this->menuComponents[$index];
+    }
+
+    public function print()
+    {
+        echo "{$this->name}\n";
+        echo "{$this->description}\n";
+        echo "------------------------------\n";
+    }
+}
