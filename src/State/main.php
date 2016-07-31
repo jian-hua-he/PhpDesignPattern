b105da82f284585aa15806e3bcb9e9c9141df521
Jian-Hua, He <game09116@gmail.com>
2016-07-31 23:38:37 +0800

    Put all together

--- src/State/main.php
+++ src/State/main.php
@@ -2,4 +2,33 @@
 
 require '../../vendor/autoload.php';
 
-echo "State Pattern\n";
+use Src\State\Machines\GumballMachine;
+use Src\State\States\HasQuarterState;
+use Src\State\States\NoQuarterState;
+use Src\State\States\SoldOutState;
+use Src\State\States\SoldState;
+
+$gumballMachine = new GumballMachine(5);
+$gumballMachine->printStatus();
+
+$gumballMachine->insertQuarter();
+$gumballMachine->turnCrank();
+$gumballMachine->dispense();
+$gumballMachine->printStatus();
+
+$gumballMachine->insertQuarter();
+$gumballMachine->ejectQuarter();
+$gumballMachine->turnCrank();
+$gumballMachine->dispense();
+$gumballMachine->printStatus();
+
+$gumballMachine->insertQuarter();
+$gumballMachine->turnCrank();
+$gumballMachine->ejectQuarter();
+$gumballMachine->printStatus();
+
+$gumballMachine->dispense();
+$gumballMachine->printStatus();
+
+$gumballMachine->insertQuarter();
+$gumballMachine->printStatus();
